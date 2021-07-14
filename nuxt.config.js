91eb288b79fead3ce7719531e8950e210e2a288a
phamhongphuc <phamhongphuc.info@gmail.com>
2021-07-14 14:36:57 +0700

    [Build pass] `@nuxtjs/tailwindcss@4.2.1` + `jit`

---
+++ nuxt.config.js
@@ -0,0 +1,29 @@
+const config = {
+    ssr: false,
+    target: "static",
+    tailwindcss: {
+        configPath: "~/tailwind.config.js",
+        cssPath: "~/assets/tailwind.css",
+    },
+    buildModules: [
+        //
+        "@nuxtjs/tailwindcss",
+        // "nuxt-vite",
+    ],
+    vite: {
+        server: {
+            fs: { strict: false },
+        },
+        vue: {},
+    },
+    plugins: [],
+    build: {
+        postcss: {
+            preset: {
+                autoprefixer: {},
+            },
+        },
+    },
+};
+
+export default config;
